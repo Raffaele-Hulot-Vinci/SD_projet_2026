5ff878d579a4c907caf44c29e19005dfec47f2f8
Raffaele Hulot <raffaele.hulot@student.vinci.be>
2026-03-19 11:49:22 +0100

    fix: Localisation wasn't staged for last commit

--- Localisation.java
+++ Localisation.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
 
 public class Localisation {
+    private int id, latitude, longitude, altitude;
+    private String nom;
+
+    public Localisation(int id, int latitude, int longitude, int altitude, String nom) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitude;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public String getNom() {
+        return nom;
+    }
 }
