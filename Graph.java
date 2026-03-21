794b231e5a6eb55e3c6a65bd1ba64f6d571cff3e
dimitrios-kalogiannis-vinci <dimitrios.kalogiannis@student.vinci.be>
2026-03-21 18:52:33 +0100

    attribut, constructeur et algorithme 1

--- Graph.java
+++ Graph.java
@@ -1,18 +1,59 @@
-import java.util.Deque;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
 
 	//ATTRIBUT ?
 	//TODO
+    Map<Long, Localisation> nodes;
+    Map<Localisation, HashSet<Rue>> localisationListMap;
 
     public Graph(String localisations, String roads)  {
         //TODO
+        nodes = new HashMap<>();
+        localisationListMap = new HashMap<>();
+
+        Scanner scanL = new Scanner(localisations);
+        while (scanL.hasNext()){
+            String[] rawL = scanL.next().split(",");
+            int id = Integer.parseInt(rawL[0]);
+            String nom = rawL[1];
+            double latitude = Double.parseDouble(rawL[2]);
+            double longitude = Double.parseDouble(rawL[3]);
+            double altitude = Double.parseDouble(rawL[4]);
+            Localisation localisation = new Localisation(id, latitude, longitude, altitude, nom);
+            localisationListMap.put(localisation, new HashSet<Rue>());
+        }
+
+
     }
 
     public Localisation[] determinerZoneInondee(long[] idsOrigin,double epsilon) {
+        Set<Localisation> visited = new HashSet<>();
+        Queue<Localisation> queue = new LinkedList<>();
+        List<Localisation> result = new ArrayList<>();
+
+        for (long l : idsOrigin) {
+            Localisation start = nodes.get(idsOrigin);
+            queue.add(start);
+            visited.add(start);
+        }
+
+        while (!queue.isEmpty()){
+            Localisation current = queue.poll();
+            result.add(current);
+            for (Rue rue : localisationListMap.get(current)) {
+                Localisation neighbor = rue.getArrive();
+                if(!visited.contains(neighbor)){
+                    if(neighbor.getAltitude() <= current.getAltitude() + epsilon){
+                        visited.add(neighbor);
+                        queue.add(neighbor);
+                    }
+                }
+                
+            }
+        }
         //TODO
-		return null ;
+		return result.toArray(new Localisation[0]);
     }
 
     public Deque<Localisation> trouverCheminLePlusCourtPourContournerLaZoneInondee(long idOrigin, long idDestination, Localisation[] floodedZone) {
